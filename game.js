6d08c538cfbd0214111176db49ada307ac9560bd
MiritGispan <123568318+MiritGispan@users.noreply.github.com>
2023-01-26 21:00:59 +0200

    Final Delivery Thursday 21:00

--- game.js
+++ game.js
@@ -1,25 +1,38 @@
+
 'use strict'
- 
+console.log('Sprint 1')
+
 const MINE = '💣'
-const EMPTY = '|_|'
+const EMPTY = 0
+const btnMain = '😊'
+
+const GAME_BOARD_CELL = '⬜️'
+const NEIGHBORS_ICONS = ['⏹', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣']
+var gMinesArray = []
+var gFlagsArray = []
+var timerInterval
+
+
 const gGame = {
-    isOn: false, //Boolean, when true we let the user play
-    shownCount: 0, //How many cells are shown
-    markedCount: 0, //How many cells are marked (with a flag)
-    secsPassed: 0 //How many seconds passed
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
 }
 const gLevel = {
     SIZE: 4,
     MINES: 2
-   }//'Beginner';
-   
-   
+}
 var gBoard
+var gameBoard
 var gIsGameOn
+
 function init() {
     console.log('game started')
     gBoard = buildBoard()
-    renderBoard(gBoard)
+    setMinesNegsCount(gBoard)
+    gameBoard = buildGameBoard()
+    renderBoard(gameBoard)
     gGame.isOn = true
 }
 function buildBoard() {
@@ -30,37 +43,86 @@ function buildBoard() {
             board[i][j] = EMPTY
         }
     }
-    const randNum=getRandomNubers(0,gLevel.SIZE-1)
-    for(var i=0; i<randNum.length; i++){
+
+    gMinesArray = []
+    const randNum = getRandomNubers(0, gLevel.SIZE - 1)
+    for (var i = 0; i < randNum.length; i++) {
         board[randNum[i][0]][randNum[i][1]] = MINE
+        gMinesArray.push(randNum[i])
     }
     return board
 }
-function getRandomNubers(min, max) {
-    return [[Math.floor(Math.random() * (max - min + 1)) + min,Math.floor(Math.random() * (max - min + 1)) + min],[Math.floor(Math.random() * (max - min + 1)) + min,Math.floor(Math.random() * (max - min + 1)) + min]];
-}
-//Game ends when all mines are marked, and all the other cells are shown
-function checkGameOver() { 
-}
-//When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors. 
-//NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
-function expandShown(board, elCell, i, j){
-}
-
-
-function setMinesNegsCount(board) {
-
-    placed = 0;
-    if (
-        i = Math.floor(Math.random() * columns * rows))  
-        if (board[i] != 'mine')      
-        {
-            board[i] = 'mine';        
-            placed++;       
+function buildGameBoard() {
+    const board = []
+    for (var i = 0; i < gLevel.SIZE; i++) {
+        board.push([])
+        for (var j = 0; j < gLevel.SIZE; j++) {
+            board[i][j] = GAME_BOARD_CELL
         }
     }
-     while (placed < MINE);  {
-        
-     }
+    return board
+}
 
-
+function checkGameOver() {
+    if (gMinesArray.length === gFlagsArray.length) {
+        for (var i = 0; i < gFlagsArray.length; i++) {
+            if (!DoesArrayContionsArray(gMinesArray, gFlagsArray[i])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+function setMinesNegsCount(board) {
+    console.log(gBoard)
+    for (var k = 0; k < gMinesArray.length; k++) {
+        var firstIndex = gMinesArray[k][0]
+        var secondIndex = gMinesArray[k][1]
+        for (var i = firstIndex - 1; i <= firstIndex + 1; i++) {
+            if (i < 0 || i >= gBoard.length) continue
+            for (var j = secondIndex - 1; j <= secondIndex + 1; j++) {
+                if (i === firstIndex && j === secondIndex) continue
+                if (j < 0 || j >= gBoard[i].length) continue
+                if (gBoard[i][j] !== MINE) {
+                    gBoard[i][j]++
+                }
+            }
+        }
+    }
+}
+function checkgBoardCellContent(i, j) {
+    return gBoard[i][j];
+}
+function setGameOver() {
+    gGame.isOn = false
+}
+function changeLevel(level) {
+    if (level === 1) {
+        gLevel.SIZE = 4,
+            gLevel.MINES = 2
+    }
+    else if (level === 2) {
+        gLevel.SIZE = 8,
+            gLevel.MINES = 14
+    }
+    else if (level === 3) {
+        gLevel.SIZE = 12,
+            gLevel.MINES = 32
+    }
+    init();
+}
+function addFlag(i, j) {
+    gFlagsArray.push([i, j])
+    const isGameFinished = checkGameOver()
+    if (isGameFinished) {
+        setGameOver()
+    }
+}
+function removeFlag(i, j) {
+    const indexOfCell = gFlagsArray.indexOf([i, j])
+    gFlagsArray.splice(indexOfCell, 1)
+    const isGameFinished = checkGameOver()
+    if (isGameFinished) {
+        setGameOver()
+    }
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+
 'use strict'
 
 function renderBoard(mat) {
@@ -7,7 +8,7 @@ function renderBoard(mat) {
         for (var j = 0; j < mat[0].length; j++) {
             const cell = mat[i][j]
             const className = `cell cell-${i}-${j}`
-            strHTML += `<td class="${className}">${cell}</td>`
+            strHTML += `<td class="${className}" oncontextmenu="onRightClicked(this, ${i}, ${j})" onClick="onCellClicked(this, ${i}, ${j})">${cell}</td>`
         }
         strHTML += '</tr>'
     }
@@ -15,12 +16,84 @@ function renderBoard(mat) {
     const elContainer = document.querySelector('.board-container')
     elContainer.innerHTML = strHTML
 }
-//Count mines around each cell and set the cell's minesAroundCount.
-function setMinesNegsCount(board){
+ //Called when a cell is clicked(left click)
+function onCellClicked(elCell,i, j) {
+    const cellContent = checkgBoardCellContent(i,j);
+    if(cellContent === 0){ //cell has no neighboars
+        elCell.innerText ='⏹'
+        expandShown(i, j)
+    }
+    else if(cellContent === MINE){ //cell has mine
+        elCell.innerText = MINE
+        console.log("game-over")
+        setGameOver();
+    }
+    else{ //cell has neighboars
+        elCell.innerText =NEIGHBORS_ICONS[cellContent]
+    }
 }
- //Called when a cell is clicked
-function onCellClicked(elCell, i, j) {
+ //Called when a cell is clicked(right click)
+function onRightClicked(elCell,i, j) {
+    const isFlag =  onCellMarked(elCell)
+    if(isFlag){
+        removeFlag(i,j)
+    }
+    else{
+        addFlag(i,j)
+    }
 }
 //Called when a cell is rightclickedee how you can hide the context menu on right click
 function onCellMarked(elCell) {
+    const isFlag = elCell.innerText === '🚩'
+    if(isFlag ){
+        elCell.innerText =GAME_BOARD_CELL
+    }
+    else{
+        elCell.innerText = '🚩'
+    }
+    return isFlag
 }
+//When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
+//NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
+function expandShown(firstIndex, secondIndex){
+    for(var i = firstIndex - 1; i <= firstIndex + 1; i++){
+        if(i < 0 || i >= gBoard.length) continue
+        for(var j = secondIndex - 1; j <= secondIndex + 1; j++){
+            if(i === firstIndex && j === secondIndex) continue
+            if(j < 0 || j >= gBoard[i].length) continue
+                const cellContent = checkgBoardCellContent(i,j);
+                const elCell = document.querySelector(`.cell-${i}-${j}`)
+                elCell.innerText = NEIGHBORS_ICONS[cellContent]
+        }
+    }
+}
+function getRandomNubers(min, max) { //return array of uniqe indexs (in the size of)
+    const output =[];
+    output.push([Math.floor(Math.random() * (max - min + 1)) + min,Math.floor(Math.random() * (max - min + 1)) + min])
+    while(output.length < gLevel.MINES){
+        var arrayOfTwoRandomNumbers = [Math.floor(Math.random() * (max - min + 1)) + min,Math.floor(Math.random() * (max - min + 1)) + min]
+        if(!DoesArrayContionsArray(output,arrayOfTwoRandomNumbers)){
+            output.push(arrayOfTwoRandomNumbers)
+        }
+    }
+    return output
+}
+function DoesArrayContionsArray(a, b) { //a is an Array of Arraies , b is just an Array. exmple: a=[[0,1],[1,1]] b= [0,1]
+    for(var i=0; i<a.length; i++){
+        if(DoesArraiesAreEquals(a[i], b)){
+            return true
+        }
+  }
+  return false
+}
+function DoesArraiesAreEquals(a, b) { //a is an Array and b is an Array. exmple: a=[1,1] b= [0,1]
+    if(a.length !== b.length){
+        return false;
+    }
+    for(var i=0; i<a.length; i++){
+        if(a[i] !== b[i]){
+            return false
+        }
+    }
+    return true
+  }
